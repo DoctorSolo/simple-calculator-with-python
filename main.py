fab3e6651b7758bcce694c8d983dd0fb0c6a3906
Miguel <dasilvamigueleduardo@gmail.com>
2024-06-09 22:56:13 -0300

    add historic

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from options import Calculator
 num1 = float(input('Enter a first number: '))
 num2 = float(input('Enter a second number: '))
 
-calculator0 = Calculator(num1, num2)
+calculator0 = Calculator(num1, num2).calculate()
 
 print(calculator0)
--- options.py
+++ options.py
@@ -3,7 +3,16 @@ class Calculator:
     def __init__(self, value1:float, value2:float):
         self.value1 = value1
         self.value2 = value2
-        return self.calculate()
+    
+
+    # The historic
+    def historic(self, result: float):
+        historic = {
+            'Value1': self.value1,
+            'Value2': self.value2,
+            'Result': result
+        }
+        print (historic)
 
     # It's will chacking the option, so user don't try anything incorrect.
     def check(self):
@@ -31,12 +40,15 @@ class Calculator:
         # it match going calcule the value
         match option:
             case 1:
-                return value1 + value2
+                result = value1 + value2
             case 2:
-                return value1 - value2
+                result = value1 - value2
             case 3:
-                return value1 * value2
+                result = value1 * value2
             case 4:
-                return value1 / value2
+                result = value1 / value2
             case _:
-                return 0
+                result = 0
+        # after calculate do historic and return
+        self.historic(result)
+        return result
