6146ea3275d4c192676b3832f5e4f2e18a178e29
Miguel <dasilvamigueleduardo@gmail.com>
2024-06-09 22:10:45 -0300

    complete calculator

---
+++ main.py
@@ -0,0 +1,8 @@
+from options import Calculator
+
+num1 = float(input('Enter a first number: '))
+num2 = float(input('Enter a second number: '))
+
+calculator0 = Calculator(num1, num2)
+
+print(calculator0)
--- options.py
+++ options.py
@@ -1,36 +1,42 @@
-class Operation:
+class Calculator:
+
+    def __init__(self, value1:float, value2:float):
+        self.value1 = value1
+        self.value2 = value2
+        return self.calculate()
 
     # It's will chacking the option, so user don't try anything incorrect.
-    def check():
+    def check(self):
 
-        try:
-
-            # Here the console going send a mensage to user
-            operation = int(input('Enter a number: '))
-            print('1: to add')
-            print('2: to subtract')
-            print('3: to multiply')
-            print('4: to shared')
-
-        except ValueError:
-            while ValueError:
-                operation = int(input('Error... Please enter an number: '))
-        
-        else:
-            return operation
-        
-        return -1
+        while True:
+            try:
+                # Here the console going send a mensage to user
+                print('1: to add')
+                print('2: to subtract')
+                print('3: to multiply')
+                print('4: to shared')
+                operation = int(input('Enter a number: '))
+                return operation
+            
+            except ValueError:
+                print('Error... Please enter an number: ')
     
 
     # It's function will calculate the result
-    def operation(value1, value2):
-        option = int(check())
+    def calculate(self):
+        value1 = self.value1
+        value2 = self.value2
+
+        option = self.check()
         # it match going calcule the value
         match option:
             case 1:
                 return value1 + value2
-        return 0
-
-
-    def __init__(self, value1, value2):
-        operation(value1, value2)
+            case 2:
+                return value1 - value2
+            case 3:
+                return value1 * value2
+            case 4:
+                return value1 / value2
+            case _:
+                return 0
